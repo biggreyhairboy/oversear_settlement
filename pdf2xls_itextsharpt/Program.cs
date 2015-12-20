de73a48f0b6df25cbdb1ddfa2cd80c861c201112
biggreyhairboy <biggreyhairboy@gmail.com>
2015-12-20 23:02:54 +0800

    开机后重新上传dll

--- pdf2xls_itextsharpt/Program.cs
+++ pdf2xls_itextsharpt/Program.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Reflection;
+
 using log4net;
 using iTextSharp.text.pdf;
 using iTextSharp.text;
 using iTextSharp.text.pdf.codec;
 using iTextSharp.text.pdf.parser;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace pdf2xls_itextsharpt
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
@@ -23,7 +27,7 @@ namespace pdf2xls_itextsharpt
             //PdfWriter.GetInstance(doc, new System.IO.FileStream(@"d:\\settlement\\bills\\RSD20151120-51028B（追保用）.pdf", FileMode.Open));
 
             //PdfDocument pdfdoc = new PdfDocument("settlefile");
-            string FileName = @"c:\settlement\bills\RSD20151120-51028B（追保用）";
+            string FileName = @"RSD20151120-51028B（追保用）";
             PdfReader reader = new PdfReader(FileName + ".pdf");
 
             string text = string.Empty;
@@ -41,12 +45,37 @@ namespace pdf2xls_itextsharpt
             StreamWriter sw = new StreamWriter(fs, Encoding.GetEncoding("GB2312"));
             sw.Write(text);
             sw.Close();
+            fs.Close();
+
+            FileStream rfs = new FileStream(FileName + ".txt", FileMode.Open);
+            StreamReader sr = new StreamReader(rfs, Encoding.GetEncoding("GB2312"));
+            Application app = new Application();
+            Workbooks wkbs = app.Workbooks;
+           // _Workbook wkb = wkbs.Add(FileName + ".xlsx");
+            _Workbook wkb = wkbs.Add("test.xlsx");
+
+            Sheets shs = wkb.Sheets;
+            
+
+            _Worksheet wsh = (_Worksheet)shs.get_Item(1);
+
+            String line = sr.ReadLine();
+            
+            while(line != null)
+            {
+                wsh.Cells[1, 1] = line;
+                break;
+            }
+            wkb.SaveAs(@".\test.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            wkb.Close();
+            wkbs.Close();
+            app.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            app = null;
+
             reader.Close();
 
             Console.ReadKey();
-
-            
-
         }
     }
 }
